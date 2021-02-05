1736f9f362f8d68fcaf713db40df0ccfd3dba3f3
AaronLin <1546848781@qq.com>
2021-02-05 09:09:25 +0800

    03 数组中的重复数字

--- src/main/java/_03FindRepeatNumber.java
+++ src/main/java/_03FindRepeatNumber.java
@@ -12,6 +12,7 @@ public class _03FindRepeatNumber {
     // 长度为 n 的数组 nums 里的所有数字都在 0～n-1 
     class Solution {
         // 1. HashSet 时间空间：O(n) O(n)
+        // 8 ms	47.5 MB
         public int findRepeatNumber1(int[] nums) {
             Set<Integer> set = new HashSet<Integer>();
             for (int num : nums) {
@@ -25,6 +26,7 @@ public class _03FindRepeatNumber {
         }
 
         // 2. 数组标记
+        // 	1 ms	46 MB
         public int findRepeatNumber2(int[] nums) {
             int[] temp = new int[nums.length];
             for (int num : nums) {
@@ -38,13 +40,34 @@ public class _03FindRepeatNumber {
         }
 
         // 数组交换
+        // 0 ms	46.2 MB
         public int findRepeatNumber(int[] nums) {
-            // 0,1,2,2
-            for (int num : nums) {
-                if (nums[num] != num) {
-                    int value = nums[num];
-                    nums[num] = nums[value];
-                    nums[value] = value;
+            // 1,0,1,2,2
+            /*
+            比如 nums[0] 值如果为0 就不变
+            如果 nums[0] 值为1 那就把它的值与 nums[1] 的交换
+            保证 nums[index] = index
+             */
+
+            for (int i = 0; i < nums.length; i++) {
+                int temp;
+                // 
+                if (nums[i] != i) {
+                    if (nums[nums[i]] == nums[i]) {
+                        return nums[i];
+                    }
+                    /*
+                    错误交换！   
+                    temp = nums[i];
+                    nums[i] = nums[nums[i]];
+                    // 这个时候 nums[i] 已经被修改了
+                    nums[nums[i]] = temp;
+                    
+                     */
+
+                    temp = nums[i];
+                    nums[i] = nums[temp];
+                    nums[temp] = temp;
                 }
             }
             return -1;
@@ -56,22 +79,25 @@ public class _03FindRepeatNumber {
     public void testRunner() {
         Solution s = new Solution();
         int[] arr = {2, 3, 1, 0, 2, 5, 3};
-        Assert.assertEquals(s.findRepeatNumber(arr), 2);
+        Assert.assertEquals(2, s.findRepeatNumber(arr));
 
         arr = new int[]{};
-        Assert.assertEquals(s.findRepeatNumber(arr), -1);
+        Assert.assertEquals(-1, s.findRepeatNumber(arr));
 
         arr = new int[]{0};
-        Assert.assertEquals(s.findRepeatNumber(arr), -1);
+        Assert.assertEquals(-1, s.findRepeatNumber(arr));
 
         arr = new int[]{0, 1, 2, 3, 4};
-        Assert.assertEquals(s.findRepeatNumber(arr), -1);
+        Assert.assertEquals(-1, s.findRepeatNumber(arr));
 
         arr = new int[]{1, 1};
-        Assert.assertEquals(s.findRepeatNumber(arr), 1);
+        Assert.assertEquals(1, s.findRepeatNumber(arr));
 
         arr = new int[]{0, 1, 1};
-        Assert.assertEquals(s.findRepeatNumber(arr), 1);
+        Assert.assertEquals(1, s.findRepeatNumber(arr));
+
+        arr = new int[]{2, 3, 1, 0, 2, 5, 3};
+        Assert.assertEquals(2, s.findRepeatNumber(arr));
 
     }
 
